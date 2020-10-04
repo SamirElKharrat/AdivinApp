f8a30aa8ead31fbe06b40698f4dcc0ff93762223
SamirElKharrat <samirelkharrat@gmail.com>
2020-10-04 13:54:00 +0100

    añadimos  fallo, acierto y error
    Fallo = con ello cada vez que fallemos nos saldra un mensaje diciendonos si el numero esta mayor o menor del que hayamos puesto
    
    Acierto = Nos indicara que hemos acertado el número y en cuentos intentos lo hemos logrado
    
    Error = ¡SOLO! saldra este mensaje si ponemos algún dato que no sea un número

--- src/main/java/dad/adivinapp/Numeros.java
+++ src/main/java/dad/adivinapp/Numeros.java
@@ -12,9 +12,8 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+public class Numeros extends Application  {
 
-public class Numeros {
-	
 	private Button botonComprobar;
 	private TextField tfNumero;
 	private int numeroAleatorio;
@@ -22,16 +21,48 @@ public class Numeros {
 	private final int MIN = 0;
 	private final int MAX = 100;
 
-}
 
 
-private void Comienzo() {
-	ComienzoPartida();
-	intentos = 0;
-}
+	private void Comienzo() {
+		numeroAleatorio();
+		intentos = 0;
+	}
+
+	private void numeroAleatorio() {
+		numeroAleatorio = (int) (Math.random() * (MAX - MIN + 1) + MIN);
+
+	}
+
+	private void fallo(int menormayor, int numero) {
+		String mensaje = "El número es " + ((menormayor == 1) ? "menor" : "mayor") + " que " + numero;
+		Alert alert = new Alert(AlertType.WARNING);
+		alert.setTitle("AdivinApp");
+		alert.setHeaderText("¡Fallo!");
+		alert.setContentText(mensaje);
+
+		alert.showAndWait();
+
+	}
+
+	private void acierto() {
+
+		Alert alert = new Alert(AlertType.INFORMATION);
+		alert.setTitle("AdivinApp");
+		alert.setHeaderText("Lo has conseguido!");
+		alert.setContentText("En solo " + intentos + " intentos.");
+
+		alert.showAndWait();
+
+	}
+
+	private void error() {
+		Alert alert = new Alert(AlertType.ERROR);
+		alert.setTitle("AdivinApp");
+		alert.setHeaderText("Error");
+		alert.setContentText("El número introducido no es correcto");
+
+		alert.showAndWait();
+	}
 
 
-private void numeroAleatorio() {
-	numeroAleatorio = (int) (Math.random() * (MAX - MIN + 1) + MIN);
-	
 }
